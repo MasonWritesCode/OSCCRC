MasonWritesCode/OSCCRC

commit f4d72333bc41951c7301ccfffab7fe6bb8714640

Increase Unity version to 2018.2.18f1 . Improvements to map file system

OSCCRC/Assets/Scripts/Game/GameMap.cs
@@ -140,14 +140,8 @@ public class GameMap : MonoBehaviour
     public bool importMap(StreamReader fin)
     {
         // Delete allocated game objects, since we are creating new ones
-        for (int j = 0; j < mapHeight; ++j)
-        {
-            for (int i = 0; i < mapWidth; ++i)
-            {
-                // North and east walls will be removed by changing the south and west of it's partner tiles
-                mapTiles[j, i].walls.south = mapTiles[j, i].walls.west = false;
-            }
-        }
+        // Removing walls will be handled at the same point in code where they are placed
+
         // We aren't currently keeping track of mice or cats, so destroy all children with a GridMovement attached
         List<GridMovement> deadMeat = new List<GridMovement>();
         GetComponentsInChildren<GridMovement>(true, deadMeat);
@@ -183,55 +177,49 @@ public class GameMap : MonoBehaviour
             generateMap(newMapHeight, newMapWidth);
         }
 
-        // Add stuff onto the tiles
+        // Old objects are gone, so now Add stuff onto the tiles
         for (int j = 0; j < mapHeight; ++j)
         {
             for (int i = 0; i < mapWidth; ++i)
             {
-                MapTile.TileImprovement tileImprovement = (MapTile.TileImprovement)int.Parse(fin.ReadLine());
-                if (tileImprovement != MapTile.TileImprovement.None)
+                // An underscore indicates there is no improvement
+                if (fin.Peek() != '_')
                 {
-                    mapTiles[j, i].improvementDirection = (Directions.Direction)int.Parse(fin.ReadLine());
-                }
-                MapTile.TileImprovement movingObj = (MapTile.TileImprovement)int.Parse(fin.ReadLine());
-                if (movingObj != MapTile.TileImprovement.None)
-                {
-                    mapTiles[j, i].movingObjDirection = (Directions.Direction)int.Parse(fin.ReadLine());
+                    MapTile.TileImprovement tileImprovement = (MapTile.TileImprovement)int.Parse(fin.ReadLine());
+                    if (tileImprovement != MapTile.TileImprovement.None)
+                    {
+                        mapTiles[j, i].improvementDirection = (Directions.Direction)int.Parse(fin.ReadLine());
+                    }
+                    MapTile.TileImprovement movingObj = (MapTile.TileImprovement)int.Parse(fin.ReadLine());
+                    if (movingObj != MapTile.TileImprovement.None)
+                    {
+                        mapTiles[j, i].movingObjDirection = (Directions.Direction)int.Parse(fin.ReadLine());
 
-                    if (movingObj == MapTile.TileImprovement.Mouse)
-                    {
-                        placeMouse(mapTiles[j, i].transform.position.x, mapTiles[j, i].transform.position.z, mapTiles[j, i].movingObjDirection);
+                        if (movingObj == MapTile.TileImprovement.Mouse)
+                        {
+                            placeMouse(mapTiles[j, i].transform.position.x, mapTiles[j, i].transform.position.z, mapTiles[j, i].movingObjDirection);
+                        }
+                        else if (movingObj == MapTile.TileImprovement.Cat)
+                        {
+                            placeCat(mapTiles[j, i].transform.position.x, mapTiles[j, i].transform.position.z, mapTiles[j, i].movingObjDirection);
+                        }
                     }
-                    else if (movingObj == MapTile.TileImprovement.Cat)
-                    {
-                        placeCat(mapTiles[j, i].transform.position.x, mapTiles[j, i].transform.position.z, mapTiles[j, i].movingObjDirection);
-                    }
+                    mapTiles[j, i].improvement = tileImprovement;
+                    mapTiles[j, i].movingObject = movingObj;
                 }
-                mapTiles[j, i].improvement = tileImprovement;
-                mapTiles[j, i].movingObject = movingObj;
+                else
+                {
+                    // Pass by the underscore
+                    fin.ReadLine();
+                }
 
                 if (i == 0 || j == 0 || (i + j) % 2 == 0)
                 {
                     int wallsValue = int.Parse(fin.ReadLine());
-                    if (wallsValue != 0)
-                    {
-                        if ((wallsValue >> 0 & 1) == 1)
-                        {
-                            mapTiles[j, i].walls.north = true;
-                        }
-                        if ((wallsValue >> 1 & 1) == 1)
-                        {
-                            mapTiles[j, i].walls.east = true;
-                        }
-                        if ((wallsValue >> 2 & 1) == 1)
-                        {
-                            mapTiles[j, i].walls.south = true;
-                        }
-                        if ((wallsValue >> 3 & 1) == 1)
-                        {
-                            mapTiles[j, i].walls.west = true;
-                        }
-                    }
+                    mapTiles[j, i].walls.north = (wallsValue >> 0 & 1) == 1;
+                    mapTiles[j, i].walls.east = (wallsValue >> 1 & 1) == 1;
+                    mapTiles[j, i].walls.south = (wallsValue >> 2 & 1) == 1;
+                    mapTiles[j, i].walls.west = (wallsValue >> 3 & 1) == 1;
                 }
             }
         }
@@ -251,16 +239,26 @@ public class GameMap : MonoBehaviour
             {
                 MapTile tile = mapTiles[j, i];
 
-                fout.WriteLine((int)tile.improvement);
-                if (tile.improvement != MapTile.TileImprovement.None)
+                // We can combine these two into a single character if they are the same. Thus a completely blank default tile will be indicated simply by only an underscore
+                // This actually only saves two characters in this sistuation though, as a blank tile before was two single digit numbers (each with a new line)
+                // 2 characters per tile is actually pretty significant relatively though
+                if (tile.improvement == MapTile.TileImprovement.None && tile.movingObject == MapTile.TileImprovement.None)
                 {
-                    fout.WriteLine((int)tile.improvementDirection);
+                    fout.WriteLine('_');
                 }
-
-                fout.WriteLine((int)tile.movingObject);
-                if (tile.movingObject != MapTile.TileImprovement.None)
+                else
                 {
-                    fout.WriteLine((int)tile.movingObjDirection);
+                    fout.WriteLine((int)tile.improvement);
+                    if (tile.improvement != MapTile.TileImprovement.None)
+                    {
+                        fout.WriteLine((int)tile.improvementDirection);
+                    }
+
+                    fout.WriteLine((int)tile.movingObject);
+                    if (tile.movingObject != MapTile.TileImprovement.None)
+                    {
+                        fout.WriteLine((int)tile.movingObjDirection);
+                    }
                 }
 
                 if (i == 0 || j == 0 || (i + j) % 2 == 0)

OSCCRC/Assets/Maps/Custom.meta
@@ -1,5 +1,6 @@
 fileFormatVersion: 2
-guid: e681699cb1f12344497b5391d8ff6009
+guid: 9a41710ee631d0842bec9819497b6dd0
+folderAsset: yes
 DefaultImporter:
   externalObjects: {}
   userData: 

OSCCRC/Assets/Maps/File Name.stage
@@ -1,293 +1,0 @@
-1
-Stage
-Default
-Default
-0
-0
-0
-0
-9
-12
-0
-0
-12
-0
-0
-4
-0
-0
-4
-0
-0
-4
-0
-0
-4
-0
-0
-4
-0
-0
-4
-0
-0
-4
-0
-0
-4
-0
-0
-4
-0
-0
-4
-0
-0
-6
-0
-0
-8
-0
-0
-0
-0
-0
-0
-0
-0
-0
-0
-0
-0
-0
-0
-0
-0
-0
-0
-0
-0
-0
-0
-0
-0
-0
-0
-0
-2
-0
-0
-8
-0
-0
-0
-0
-0
-0
-0
-0
-0
-0
-0
-0
-0
-0
-0
-0
-0
-0
-0
-0
-0
-0
-0
-0
-0
-0
-0
-0
-0
-8
-0
-0
-0
-0
-0
-0
-0
-0
-0
-0
-0
-0
-0
-0
-0
-0
-0
-0
-0
-0
-0
-0
-0
-0
-0
-0
-0
-2
-0
-0
-8
-0
-0
-0
-0
-0
-0
-0
-0
-0
-0
-0
-0
-0
-0
-0
-0
-5
-2
-0
-0
-0
-0
-0
-0
-0
-0
-0
-0
-0
-0
-8
-0
-0
-0
-0
-0
-0
-0
-0
-0
-0
-0
-0
-0
-0
-0
-0
-0
-0
-0
-0
-0
-0
-0
-0
-0
-0
-0
-2
-0
-0
-8
-0
-0
-0
-0
-0
-0
-0
-0
-0
-0
-0
-0
-0
-0
-0
-0
-0
-0
-0
-0
-0
-0
-0
-0
-0
-0
-0
-0
-0
-8
-0
-5
-2
-0
-0
-0
-0
-0
-0
-0
-0
-0
-0
-0
-0
-0
-0
-5
-2
-0
-0
-0
-0
-0
-0
-0
-0
-0
-0
-2
-0
-0
-9
-0
-0
-0
-0
-1
-0
-0
-0
-0
-1
-0
-0
-0
-0
-1
-0
-0
-0
-0
-1
-0
-0
-0
-0
-1
-0
-0

OSCCRC/Assets/Maps/New.meta
@@ -1,5 +1,6 @@
 fileFormatVersion: 2
-guid: 650cea2cb9824964ab49885ea5d3391b
+guid: 39cc908bcf6e0e04f9754e5fad21d096
+folderAsset: yes
 DefaultImporter:
   externalObjects: {}
   userData: 

OSCCRC/Assets/Maps/Retro/Stage2.stage
@@ -8,286 +8,181 @@ Default
 2
 9
 12
-0
-0
+_
 12
-0
-0
+_
 4
-0
-0
+_
 4
-0
-0
+_
 4
-0
-0
+_
 4
-0
-0
+_
 4
-0
-0
+_
 4
-0
-0
+_
 4
-0
-0
+_
 4
-0
-0
+_
 4
-0
-0
+_
 4
-0
-0
+_
 6
-0
-0
+_
 8
+_
 0
+_
+_
 0
-0
-0
-0
-0
-0
-0
-0
-0
-0
-0
+_
+_
 0
 0
 5
 1
+_
 0
+_
+_
 0
-0
-0
-0
-0
-0
-0
-0
-0
-0
-0
+_
+_
 2
-0
-0
+_
 8
+_
+_
 0
+_
+_
 0
+_
+_
 0
+_
+_
 0
+_
+_
 0
-0
-0
-0
-0
-0
-0
-0
-0
-0
-0
-0
-0
-0
-0
-0
-0
-0
-0
-0
-0
-0
-0
-0
-0
+_
+_
 8
+_
 0
+_
+_
 0
+_
+_
 0
-0
-0
-0
-0
-0
-0
-0
-0
-0
-0
-0
-0
+_
 1
 8
 0
 0
+_
+_
 0
-0
-0
-0
-0
-0
-0
-0
-0
+_
+_
 2
-0
-0
+_
 8
+_
+_
 0
+_
+_
 0
+_
+_
 0
+_
+_
 0
+_
+_
 0
-0
-0
-0
-0
-0
-0
-0
-0
-0
-0
-0
-0
-0
-0
-0
-0
-0
-0
-0
-0
-0
-0
-0
-0
+_
+_
 8
+_
 0
-0
-0
-0
-0
+_
 2
 8
 0
 0
+_
+_
 0
+_
+_
 0
+_
+_
 0
-0
-0
-0
-0
-0
-0
-0
-0
-0
-0
-0
-0
-0
-0
-0
-0
+_
+_
 2
-0
-0
+_
 8
+_
+_
 0
+_
+_
 0
+_
+_
 0
+_
+_
 0
+_
+_
 0
-0
-0
-0
-0
-0
-0
-0
-0
-0
-0
-0
-0
-0
-0
-0
-0
-0
-0
-0
-0
-0
-0
-0
-0
+_
+_
 8
+_
 0
+_
+_
 0
+_
+_
 0
+_
+_
 0
+_
+_
 0
-0
-0
-0
-0
-0
-0
-0
-0
-0
-0
-0
-0
-0
-0
-0
-0
-0
-0
-0
-0
-0
-0
+_
+_
 2
-0
-0
+_
 9
-0
-0
-0
-0
+_
+_
 1
-0
-0
-0
-0
+_
+_
 1
-0
-0
-0
-0
+_
+_
 1
-0
-0
-0
-0
+_
+_
 1
-0
-0
-0
-0
+_
+_
 1
-0
-0
+_

commit c5e4b10855947f759bc00c931c94d182b4bc625b

Fix bug where old map data wasn't discarded when loading a new map in editor
This bug was introduced by commit f4d72333bc41951c7301ccfffab7fe6bb8714640

OSCCRC/Assets/Scripts/Game/GameMap.cs
@@ -219,6 +219,9 @@ public class GameMap : MonoBehaviour
                 }
                 else
                 {
+                    mapTiles[j, i].improvement = MapTile.TileImprovement.None;
+                    mapTiles[j, i].movingObject = MapTile.TileImprovement.None;
+
                     // Pass by the underscore
                     fin.ReadLine();
                 }