be17a1bc6de5b805569418d6ead717bcda991dfa
Alex Disibio <alexdisibio@gmail.com>
2019-02-19 22:28:02 -0500

    Move Walls class outside of MapTile class
    Even though it should only be owned by a maptile, MapTile was too big as a single file and I guess it is fine not being a partial class

--- OSCCRC/Assets/Scripts/Game/MapTile.cs
+++ OSCCRC/Assets/Scripts/Game/MapTile.cs
@@ -9,136 +9,15 @@ public class MapTile : MonoBehaviour
     // Material resource names will have to be manually added and adjusted in the start function as tile improvements change
     public enum TileImprovement { None, Hole, Goal, Spawner, Direction, Mouse, Cat }
 
-    // Class that holds wall information that will be associated with a tile.
-    // This is within the MapTile class because only a tile should access walls directly.
-    public class Walls
-    {
-        public bool this[Directions.Direction wallID] { get { return m_walls[wallID] != null; } set { changeWall(wallID, value); } }
-        // Probably should use indexer only and make the direction properties deprecated?
-        public bool north { get { return m_walls[Directions.Direction.North] != null; } set { changeWall(Directions.Direction.North, value); } }
-        public bool east { get { return m_walls[Directions.Direction.East] != null; } set { changeWall(Directions.Direction.East, value); } }
-        public bool south { get { return m_walls[Directions.Direction.South] != null; } set { changeWall(Directions.Direction.South, value); } }
-        public bool west { get { return m_walls[Directions.Direction.West] != null; } set { changeWall(Directions.Direction.West, value); } }
-
-        private Vector3 origin;
-        private GameMap map;
-        private readonly int maxHeightIndex, maxWidthIndex;
-        private Dictionary<Directions.Direction, Transform> m_walls = new Dictionary<Directions.Direction, Transform>(4);
-
-        public Walls(GameMap parentMap, Vector3 originPos)
-        {
-            map = parentMap;
-            origin = originPos;
-            maxHeightIndex = map.mapHeight - 1;
-            maxWidthIndex = map.mapWidth - 1;
-
-            m_walls.Add(Directions.Direction.North, null);
-            m_walls.Add(Directions.Direction.East, null);
-            m_walls.Add(Directions.Direction.South, null);
-            m_walls.Add(Directions.Direction.West, null);
-        }
-
-        // Removes all four walls if they exist
-        public void clear()
-        {
-            foreach (Directions.Direction key in m_walls.Keys)
-            {
-                map.destroyWall(m_walls[key]);
-                m_walls[key] = null;
-            }
-        }
-
-        // Walls are shared by the two tiles they touch, so we must inform the other tile to accept the created wall as its own
-        private void changeWall(Directions.Direction wallID, bool isCreating)
-        {
-            if (isCreating && m_walls[wallID] == null)
-            {
-                m_walls[wallID] = map.createWall(origin.x, origin.z, wallID);
-            }
-            else if (!isCreating && m_walls[wallID] != null)
-            {
-                map.destroyWall(m_walls[wallID]);
-                m_walls[wallID] = null;
-            }
-            else
-            {
-                // Wall is not able to be created or destroyed
-                return;
-            }
-
-            // Determine where the other wall is if it exists
-            float otherTileX = origin.x, otherTileZ = origin.z;
-            if (wallID == Directions.Direction.North)
-            {
-                otherTileZ += map.tileSize;
-                if (otherTileZ > (maxHeightIndex * map.tileSize))
-                {
-                    otherTileZ = 0;
-                }
-            }
-            else if (wallID == Directions.Direction.East)
-            {
-                otherTileX += map.tileSize;
-                if (otherTileX > (maxWidthIndex * map.tileSize))
-                {
-                    otherTileX = 0;
-                }
-            }
-            else if (wallID == Directions.Direction.South)
-            {
-                otherTileZ -= map.tileSize;
-                if (otherTileZ < 0)
-                {
-                    otherTileZ = maxHeightIndex * map.tileSize;
-                }
-            }
-            else if (wallID == Directions.Direction.West)
-            {
-                otherTileX -= map.tileSize;
-                if (otherTileX < 0)
-                {
-                    otherTileX = maxWidthIndex * map.tileSize;
-                }
-            }
-
-            // The companion wall always has the same existence state
-            if (Mathf.Abs(origin.z - otherTileZ) == (maxHeightIndex * map.tileSize) || Mathf.Abs(origin.x - otherTileX) == (maxWidthIndex * map.tileSize))
-            {
-                // Walls on edges share state with wrapped around tile, but they don't share the same wall object
-                map.tileAt(new Vector3(otherTileX, 0, otherTileZ)).walls.changeWall(Directions.getOppositeDir(wallID), isCreating );
-            }
-            else
-            {
-                // This can be done on a single line if you don't hate the ugliness of the ternary operator like I do
-                Transform wallSet = null;
-                if (isCreating)
-                {
-                    wallSet = m_walls[wallID];
-                }
-                map.tileAt(new Vector3(otherTileX, 0, otherTileZ)).walls.m_walls[Directions.getOppositeDir(wallID)] = wallSet;
-            }
-        }
-    }
-
-
     public TileImprovement improvement { get { return m_improvement; } set { setTileImprovement(value); } }
     public TileImprovement movingObject { get { return m_movingObject; } set { m_movingObject = value; } }
-    public Walls walls;
     public Directions.Direction improvementDirection { get { return m_improvementDir;  } set { m_improvementDir = value; Directions.rotate(ref m_tileObject, value); } }
     public Directions.Direction movingObjDirection { get { return m_movingDir; } set { m_movingDir = value; } }
+    public Walls walls;
 
     // This needs to be public so that the editor can see the texture used for an improvement
     public static Dictionary<TileImprovement, string> improvementTextures = new Dictionary<TileImprovement, string>();
     public static Dictionary<TileImprovement, string> improvementObjects = new Dictionary<TileImprovement, string>();
-
-    private TileImprovement m_improvement;
-    private TileImprovement m_movingObject;
-    private Directions.Direction m_improvementDir;
-    private Directions.Direction m_movingDir;
-    private Transform m_tileObject;
-    private int m_tileDamage;
-    private MeshRenderer m_rendererRef;
-
 
     // This static constructor is used to generate a map used to interface with resource packs
     static MapTile()
@@ -289,7 +168,7 @@ public class MapTile : MonoBehaviour
         {
             if (GameResources.objects.ContainsKey(objectName))
             {
-                m_tileObject = Instantiate(GameResources.objects[objectName], GetComponent<Transform>());
+                m_tileObject = Instantiate(GameResources.objects[objectName], transform);
                 if (improvement == TileImprovement.Direction)
                 {
                     // Directional arrows are objects that are implemented as tile objects themselves, which can cause z-fighting.
@@ -307,4 +186,11 @@ public class MapTile : MonoBehaviour
         m_improvement = improvement;
     }
 
+    private TileImprovement m_improvement;
+    private TileImprovement m_movingObject;
+    private Directions.Direction m_improvementDir;
+    private Directions.Direction m_movingDir;
+    private Transform m_tileObject;
+    private int m_tileDamage;
+    private MeshRenderer m_rendererRef;
 }
---
+++ OSCCRC/Assets/Scripts/Game/Walls.cs
@@ -0,0 +1,114 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Class that holds wall information that will be associated with a tile.
+
+public class Walls
+{
+    public bool this[Directions.Direction wallID] { get { return m_walls[wallID] != null; } set { changeWall(wallID, value); } }
+    // Probably should use indexer only and make the direction properties deprecated?
+    public bool north { get { return m_walls[Directions.Direction.North] != null; } set { changeWall(Directions.Direction.North, value); } }
+    public bool east { get { return m_walls[Directions.Direction.East] != null; } set { changeWall(Directions.Direction.East, value); } }
+    public bool south { get { return m_walls[Directions.Direction.South] != null; } set { changeWall(Directions.Direction.South, value); } }
+    public bool west { get { return m_walls[Directions.Direction.West] != null; } set { changeWall(Directions.Direction.West, value); } }
+
+    public Walls(GameMap parentMap, Vector3 originPos)
+    {
+        m_map = parentMap;
+        m_origin = originPos;
+        m_maxHeightIndex = m_map.mapHeight - 1;
+        m_maxWidthIndex = m_map.mapWidth - 1;
+
+        m_walls.Add(Directions.Direction.North, null);
+        m_walls.Add(Directions.Direction.East, null);
+        m_walls.Add(Directions.Direction.South, null);
+        m_walls.Add(Directions.Direction.West, null);
+    }
+
+    // Removes all four walls if they exist
+    public void clear()
+    {
+        foreach (Directions.Direction key in m_walls.Keys)
+        {
+            m_map.destroyWall(m_walls[key]);
+            m_walls[key] = null;
+        }
+    }
+
+    // Walls are shared by the two tiles they touch, so we must inform the other tile to accept the created wall as its own
+    private void changeWall(Directions.Direction wallID, bool isCreating)
+    {
+        if (isCreating && m_walls[wallID] == null)
+        {
+            m_walls[wallID] = m_map.createWall(m_origin.x, m_origin.z, wallID);
+        }
+        else if (!isCreating && m_walls[wallID] != null)
+        {
+            m_map.destroyWall(m_walls[wallID]);
+            m_walls[wallID] = null;
+        }
+        else
+        {
+            // Wall is not able to be created or destroyed
+            return;
+        }
+
+        // Determine where the other wall is if it exists
+        float otherTileX = m_origin.x, otherTileZ = m_origin.z;
+        if (wallID == Directions.Direction.North)
+        {
+            otherTileZ += m_map.tileSize;
+            if (otherTileZ > (m_maxHeightIndex * m_map.tileSize))
+            {
+                otherTileZ = 0;
+            }
+        }
+        else if (wallID == Directions.Direction.East)
+        {
+            otherTileX += m_map.tileSize;
+            if (otherTileX > (m_maxWidthIndex * m_map.tileSize))
+            {
+                otherTileX = 0;
+            }
+        }
+        else if (wallID == Directions.Direction.South)
+        {
+            otherTileZ -= m_map.tileSize;
+            if (otherTileZ < 0)
+            {
+                otherTileZ = m_maxHeightIndex * m_map.tileSize;
+            }
+        }
+        else if (wallID == Directions.Direction.West)
+        {
+            otherTileX -= m_map.tileSize;
+            if (otherTileX < 0)
+            {
+                otherTileX = m_maxWidthIndex * m_map.tileSize;
+            }
+        }
+
+        // The companion wall always has the same existence state
+        if (Mathf.Abs(m_origin.z - otherTileZ) == (m_maxHeightIndex * m_map.tileSize) || Mathf.Abs(m_origin.x - otherTileX) == (m_maxWidthIndex * m_map.tileSize))
+        {
+            // Walls on edges share state with wrapped around tile, but they don't share the same wall object
+            m_map.tileAt(new Vector3(otherTileX, 0, otherTileZ)).walls.changeWall(Directions.getOppositeDir(wallID), isCreating);
+        }
+        else
+        {
+            // This can be done on a single line if you don't hate the ugliness of the ternary operator like I do
+            Transform wallSet = null;
+            if (isCreating)
+            {
+                wallSet = m_walls[wallID];
+            }
+            m_map.tileAt(new Vector3(otherTileX, 0, otherTileZ)).walls.m_walls[Directions.getOppositeDir(wallID)] = wallSet;
+        }
+    }
+
+    private Vector3 m_origin;
+    private GameMap m_map;
+    private readonly int m_maxHeightIndex, m_maxWidthIndex;
+    private Dictionary<Directions.Direction, Transform> m_walls = new Dictionary<Directions.Direction, Transform>(4);
+}
