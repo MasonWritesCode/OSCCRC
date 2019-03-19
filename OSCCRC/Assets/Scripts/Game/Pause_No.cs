f6d8ce4c82c00ea8827b3d7da162f107a71e0570
Alex Disibio <alexdisibio@gmail.com>
2019-03-19 15:17:31 -0400

    Fix exception in pause menu after GameObject name change

--- OSCCRC/Assets/Scripts/Game/Pause_No.cs
+++ OSCCRC/Assets/Scripts/Game/Pause_No.cs
@@ -2,15 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
-using UnityEngine.SceneManagement;
 
 public class Pause_No : MonoBehaviour, IPointerClickHandler
 {
+    public Canvas pauseDisplay; // Editor Set
+
     public void OnPointerClick(PointerEventData pointerEventData)
     {
         if (pointerEventData.button == PointerEventData.InputButton.Left)
         {
-            Canvas pauseDisplay = GameObject.Find("PauseMenu").GetComponent<Canvas>();
             GameController m_gameController = GameObject.FindWithTag("GameController").GetComponent<GameController>();
 
             pauseDisplay.enabled = false;
