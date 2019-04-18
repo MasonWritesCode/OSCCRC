MasonWritesCode/OSCCRC

commit fa120033e53ea7b631a6c9395383ff18516850db

Remove GridMovement.isCat and use child classes instead

OSCCRC/Assets/Scripts/Game/Cat.cs
@@ -1,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// A Cat implementation of a Grid-Mover.
+
+public class Cat : GridMovement {
+
+    /*
+    void Awake()
+    {
+        speed = 4.4444f;
+        direction = Directions.Direction.North;
+    }
+    */
+
+    // Cats can destroy mice, so they check for a collision
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<Mouse>() != null)
+        {
+            m_map.destroyMouse(other.transform);
+        }
+    }
+
+    // Runs interactions with the specified tile improvement
+    protected override void interactWithImprovement(MapTile.TileImprovement improvement)
+    {
+        //check for goals and holes
+        if (tile.improvement == MapTile.TileImprovement.Goal)
+        {
+            m_map.destroyCat(transform);
+
+            return;
+        }
+        else if (tile.improvement == MapTile.TileImprovement.Hole)
+        {
+            m_map.destroyCat(transform);
+
+            return;
+        }
+        //checking for arrows
+        else if (tile.improvement == MapTile.TileImprovement.Direction)
+        {
+            direction = tile.improvementDirection;
+
+            if (direction == Directions.getOppositeDir(tile.improvementDirection))
+            {
+                tile.damageTile();
+            }
+        }
+    }
+}

OSCCRC/Assets/Resources/Default/Prefabs/Cat.prefab
@@ -155,10 +155,10 @@ GameObject:
   serializedVersion: 6
   m_Component:
   - component: {fileID: 4932463689872970}
-  - component: {fileID: 114070464262402396}
   - component: {fileID: 95781845954143718}
   - component: {fileID: 135955852886912508}
   - component: {fileID: 54199121661686834}
+  - component: {fileID: 114092590358716338}
   m_Layer: 0
   m_Name: Cat
   m_TagString: Untagged
@@ -733,7 +733,7 @@ Animator:
   m_HasTransformHierarchy: 1
   m_AllowConstantClipSamplingOptimization: 1
   m_KeepAnimatorControllerStateOnDisable: 0
---- !u!114 &114070464262402396
+--- !u!114 &114092590358716338
 MonoBehaviour:
   m_ObjectHideFlags: 1
   m_CorrespondingSourceObject: {fileID: 0}
@@ -741,12 +741,11 @@ MonoBehaviour:
   m_GameObject: {fileID: 1583404243129010}
   m_Enabled: 1
   m_EditorHideFlags: 0
-  m_Script: {fileID: 11500000, guid: 18893cb5c1faa864c9353e63cde63754, type: 3}
+  m_Script: {fileID: 11500000, guid: fb9d61a1a7a188a46ab483188abca51d, type: 3}
   m_Name: 
   m_EditorClassIdentifier: 
   speed: 4.4444
-  direction: 0
-  isCat: 1
+  direction: 1
 --- !u!135 &135955852886912508
 SphereCollider:
   m_ObjectHideFlags: 1

OSCCRC/Assets/Resources/Default/Prefabs/Mouse.prefab
@@ -70,10 +70,10 @@ GameObject:
   serializedVersion: 6
   m_Component:
   - component: {fileID: 4476613451300466}
-  - component: {fileID: 114052251384721900}
   - component: {fileID: 95502869000028644}
   - component: {fileID: 135525755073462502}
   - component: {fileID: 54332728872675912}
+  - component: {fileID: 114856738673410134}
   m_Layer: 0
   m_Name: Mouse
   m_TagString: Untagged
@@ -660,7 +660,7 @@ Animator:
   m_HasTransformHierarchy: 1
   m_AllowConstantClipSamplingOptimization: 1
   m_KeepAnimatorControllerStateOnDisable: 0
---- !u!114 &114052251384721900
+--- !u!114 &114856738673410134
 MonoBehaviour:
   m_ObjectHideFlags: 1
   m_CorrespondingSourceObject: {fileID: 0}
@@ -668,12 +668,11 @@ MonoBehaviour:
   m_GameObject: {fileID: 1468713278425226}
   m_Enabled: 1
   m_EditorHideFlags: 0
-  m_Script: {fileID: 11500000, guid: 18893cb5c1faa864c9353e63cde63754, type: 3}
+  m_Script: {fileID: 11500000, guid: 373e4dd5da2abb248bb213f57715ea6e, type: 3}
   m_Name: 
   m_EditorClassIdentifier: 
   speed: 6.6666
-  direction: 0
-  isCat: 0
+  direction: 1
 --- !u!135 &135525755073462502
 SphereCollider:
   m_ObjectHideFlags: 1

OSCCRC/Assets/Scripts/Game/Cat.cs.meta
@@ -1,0 +1,11 @@
+fileFormatVersion: 2
+guid: fb9d61a1a7a188a46ab483188abca51d
+MonoImporter:
+  externalObjects: {}
+  serializedVersion: 2
+  defaultReferences: []
+  executionOrder: 0
+  icon: {instanceID: 0}
+  userData: 
+  assetBundleName: 
+  assetBundleVariant: 

OSCCRC/Assets/Scripts/Game/Editor.cs
@@ -310,13 +310,16 @@ public class Editor : MonoBehaviour {
                     if (m_movingObjects[selectedTile] != null)
                     {
                         GridMovement gm = m_movingObjects[selectedTile].GetComponent<GridMovement>();
-                        if (gm && !gm.isCat)
+                        if (gm)
                         {
-                            m_gameMap.destroyMouse(m_movingObjects[selectedTile].transform);
-                        }
-                        else if (gm && gm.isCat)
-                        {
-                            m_gameMap.destroyCat(m_movingObjects[selectedTile].transform);
+                            if (gm is Mouse)
+                            {
+                                m_gameMap.destroyMouse(m_movingObjects[selectedTile].transform);
+                            }
+                            else if (gm is Cat)
+                            {
+                                m_gameMap.destroyCat(m_movingObjects[selectedTile].transform);
+                            }
                         }
                     }
                     m_movingObjects.Remove(selectedTile);

commit c21213f0a7f9b7863857003d048637b06aef5233

Fix mistake in commit fa12003 which caused cats to not destroy tiles

OSCCRC/Assets/Scripts/Game/Cat.cs
@@ -42,12 +42,12 @@ public class Cat : GridMovement {
         //checking for arrows
         else if (tile.improvement == MapTile.TileImprovement.Direction)
         {
-            direction = tile.improvementDirection;
-
             if (direction == Directions.getOppositeDir(tile.improvementDirection))
             {
                 tile.damageTile();
             }
+
+            direction = tile.improvementDirection;
         }
     }
 }