ecba924f42907d698c42c4cba9c42bed7bab61a3
Alex Disibio <alexdisibio@gmail.com>
2019-06-27 12:09:51 -0400

    Add button to clear completion data in dev menu

---
+++ OSCCRC/Assets/Scripts/DevMenu/UI/Menu_ClearCompletions.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Menu_ClearCompletions : MonoBehaviour, IPointerClickHandler
+{
+
+    public void OnPointerClick(PointerEventData pointerEventData)
+    {
+        CompletionTracker.unmarkAll();
+        Debug.Log("Completion Data cleared");
+    }
+}
