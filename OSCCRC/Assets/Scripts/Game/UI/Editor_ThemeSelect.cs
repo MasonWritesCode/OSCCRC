270749595014e3a3bbf772fbf98d16ded5f4cbd4
Alex Disibio <alexdisibio@gmail.com>
2019-07-06 14:21:42 -0400

    Have the editor theme selector update itself when the map changes

--- OSCCRC/Assets/Scripts/Game/UI/Editor_ThemeSelect.cs
+++ OSCCRC/Assets/Scripts/Game/UI/Editor_ThemeSelect.cs
@@ -7,37 +7,58 @@ public class Editor_ThemeSelect : MonoBehaviour {
 	void Start () {
         m_gameMap = GameObject.FindWithTag("Map").GetComponent<GameMap>();
         m_mapResources = m_gameMap.GetComponent<GameResources>();
+        m_dropdown = GetComponent<Dropdown>();
 
-        Dropdown m_dropdown = GetComponent<Dropdown>();
+        m_gameMap.mapLoaded += onMapLoaded;
+
         m_dropdown.onValueChanged.AddListener(delegate {
             onValueChange(m_dropdown);
         });
     }
-	
+
+
 	void onValueChange(Dropdown obj)
     {
         string packName = obj.captionText.text;
 
-        m_mapResources.loadResources(packName);
-
-        // Save the map into memory and reload from that save to load in the new resource pack
-        using (MemoryStream ms = new MemoryStream())
+        if (m_mapResources.resourcePack != packName)
         {
-            using (StreamWriter sw = new StreamWriter(ms))
-            {
-                m_gameMap.exportMap(sw);
+            m_mapResources.loadResources(packName);
 
-                // Closing the writer will close the memory stream, so flush and open the reader while it is alive
-                sw.Flush();
-                ms.Position = 0;
-                using (StreamReader sr = new StreamReader(ms))
+            // Save the map into memory and reload from that save to load in the new resource pack
+            using (MemoryStream ms = new MemoryStream())
+            {
+                using (StreamWriter sw = new StreamWriter(ms))
                 {
-                    m_gameMap.importMap(sr);
+                    m_gameMap.exportMap(sw);
+
+                    // Closing the writer will close the memory stream, so flush and open the reader while it is alive
+                    sw.Flush();
+                    ms.Position = 0;
+                    using (StreamReader sr = new StreamReader(ms))
+                    {
+                        m_gameMap.importMap(sr);
+                    }
                 }
             }
         }
     }
 
+
+    void onMapLoaded()
+    {
+        for (int i = 0; i < m_dropdown.options.Count; ++i)
+        {
+            if (m_dropdown.options[i].text == m_mapResources.resourcePack)
+            {
+                m_dropdown.value = i;
+                break;
+            }
+        }
+    }
+
+
     private GameMap m_gameMap;
     private GameResources m_mapResources;
+    private Dropdown m_dropdown;
 }
