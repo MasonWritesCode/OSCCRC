3901ab2d907a2770e6d007a735ee39f4eb34818a
Alex Disibio <alexdisibio@gmail.com>
2019-07-24 18:23:16 -0400

    Implement placeDirection for competitive mode

--- OSCCRC/Assets/Scripts/Game/CompetitiveGame.cs
+++ OSCCRC/Assets/Scripts/Game/CompetitiveGame.cs
@@ -2,6 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+// This is an interface between the game controller and the Competitive game mode.
+// This mode requires being passed information about the game state.
+
 public class CompetitiveGame : IGameMode
 {
     public CompetitiveGame(GameState gameStateRef)
@@ -31,10 +34,42 @@ public class CompetitiveGame : IGameMode
     }
 
 
-    public void placeDirection(MapTile tile, Directions.Direction dir, int player)
+    public void placeDirection(MapTile tile, Directions.Direction dir, int playerID)
     {
-        // TODO
-        return;
+        if (tile.improvement != MapTile.TileImprovement.None)
+        {
+            // Players cannot change an already placed tile
+            return;
+        }
+
+        const int maxPlacements = 3;
+
+        Player player = players[playerID];
+
+        // Players are limited to the 3 most recently placed
+        if (player.placements.Count >= maxPlacements)
+        {
+            Placement tileToClear = player.placements.Dequeue();
+            tileToClear.tile.improvement = MapTile.TileImprovement.None;
+        }
+
+        tile.owner = playerID;
+        tile.improvementDirection = dir;
+        tile.improvement = MapTile.TileImprovement.Direction;
+
+        Timer ptimer = new Timer();
+        ptimer.timerCompleted += () =>
+        {
+            // TODO: Start blinking or whatever at 1 second remaining. Would have the timer start a new timer?
+            tile.improvement = MapTile.TileImprovement.None;
+        };
+        ptimer.startTimer(10.0f);
+
+        Placement newPlacement;
+        newPlacement.tile = tile;
+        newPlacement.timer = ptimer;
+
+        player.placements.Enqueue(newPlacement);
     }
 
 
@@ -44,5 +79,19 @@ public class CompetitiveGame : IGameMode
         return;
     }
 
+
+    private struct Placement
+    {
+        public MapTile tile;
+        public Timer timer;
+    }
+
+    private class Player
+    {
+        public Queue<Placement> placements = new Queue<Placement>();
+        public int score = 0;
+    }
+
     private GameState m_gameState;
+    private Player[] players = new Player[4];
 }
